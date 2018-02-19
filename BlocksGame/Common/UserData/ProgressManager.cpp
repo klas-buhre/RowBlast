07674741927381f43cd341d671d0d67bf10d9878
klas-buhre <klas.buhre81@gmail.com>
2018-02-19 19:48:02 +0100

    Added level 28.

--- BlocksGame/Common/UserData/ProgressManager.cpp
+++ BlocksGame/Common/UserData/ProgressManager.cpp
@@ -18,7 +18,7 @@ ProgressManager::ProgressManager() {
     if (!LoadState()) {
         mNumStars = {0};
     }
-    mNumStars = {2, 3, 1, 3, 2, 2, 2, 1, 1, 1, 1, 1, 3, 2, 2, 1, 2, 2, 1, 3, 2, 2, 1, 3, 1, 2, 0};
+    mNumStars = {2, 3, 1, 3, 2, 2, 2, 1, 1, 1, 1, 1, 3, 2, 2, 1, 2, 2, 1, 3, 2, 2, 1, 3, 1, 2, 3, 0};
 }
 
 int ProgressManager::CalculateNumStars(int movesUsed, const StarLimits& starLimits) {
--- BlocksGame/Map/Chapters/Chapter1.cpp
+++ BlocksGame/Map/Chapters/Chapter1.cpp
@@ -31,7 +31,8 @@ namespace {
         MapLevel {.mLevelIndex = 25, .mPosition = {11.0f, -3.5f, 10.0f}},
         MapLevel {.mLevelIndex = 26, .mPosition = {15.5f, -3.5f, 10.0f}},
         MapLevel {.mLevelIndex = 27, .mPosition = {19.5f, -3.5f, 10.0f}},
-        MapLevel {.mLevelIndex = 28, .mPosition = {22.0f, -2.0f, 10.0f}}
+        MapLevel {.mLevelIndex = 28, .mPosition = {22.0f, -2.0f, 10.0f}},
+        MapLevel {.mLevelIndex = 29, .mPosition = {23.5f, 1.0f, 10.0f}}
     };
     
     const std::vector<BlockPathVolume> blockPaths {
