d9f0e1c8bdfcb5a4132076301618a5925a8c033f
klas-buhre <klas.buhre81@gmail.com>
2019-06-17 13:16:16 +0200

    Continued on the TextureAtlas.

--- Src/PhotonBeamEngine/Renderer/Common/TextureAtlas.cpp
+++ Src/PhotonBeamEngine/Renderer/Common/TextureAtlas.cpp
@@ -10,10 +10,19 @@ namespace {
     constexpr auto maxSize = 2048;
     constexpr auto minSize = 32;
     
+    struct SubTexture {
+        IVec2 mPosition;
+        const IImage* mImage;
+    };
+    
     bool ImagesAreCompatible(const SubImages& images) {
         const IImage* previousImage = nullptr;
         
         for (auto& image: images) {
+            if (image == nullptr) {
+                return false;
+            }
+            
             if (previousImage) {
                 if (image->GetFormat() != previousImage->GetFormat() ||
                     image->GetBitsPerComponent() != previousImage->GetBitsPerComponent() ||
@@ -29,10 +38,128 @@ namespace {
         return true;
     }
     
-    bool TextureFitsInAtlas(const IVec2& subTexturePosition,
-                            const IVec2& subTextureSize,
-                            const IVec2& atlasSize) {
-        return false;
+    bool TextureFits(const IVec2& subTexturePosition,
+                     const IVec2& subTextureSize,
+                     const IVec2& atlasSize) {
+        return subTexturePosition.x >= 0 && subTexturePosition.x + subTextureSize.x < atlasSize.x &&
+               subTexturePosition.y >= 0 && subTexturePosition.y + subTextureSize.y < atlasSize.y;
+    }
+    
+    bool PackVertically(std::vector<SubTexture>& subTextures, const IVec2& atlasSize) {
+        IVec2 subTexturePosition {0, 0};
+        auto xMax = 0;
+        
+        for (auto& subTexture: subTextures) {
+            auto subTextureSize = subTexture.mImage->GetSize();
+            if (!TextureFits(subTexturePosition, subTextureSize, atlasSize)) {
+                subTexturePosition = {xMax, 0};
+                if (!TextureFits(subTexturePosition, subTextureSize, atlasSize)) {
+                    return false;
+                }
+            }
+            
+            subTexture.mPosition = subTexturePosition;
+            
+            subTexturePosition.y += subTextureSize.y;
+            if (subTexturePosition.x + subTextureSize.x > xMax) {
+                xMax = subTexturePosition.x + subTextureSize.x;
+            }
+        }
+        
+        return true;
+    }
+    
+    Optional<IVec2> PackTexturesVertically(std::vector<SubTexture>& subTextures) {
+        IVec2 atlasImageSize {minSize, minSize};
+        auto counter = 1;
+        
+        while (atlasImageSize.x <= maxSize && atlasImageSize.y <= maxSize) {
+            if (PackVertically(subTextures, atlasImageSize)) {
+                return atlasImageSize;
+            }
+            
+            if (counter % 2 == 0) {
+                atlasImageSize.x *= 2;
+            } else {
+                atlasImageSize.y *= 2;
+            }
+            
+            ++counter;
+        }
+
+        return Optional<IVec2> {};
+    }
+
+    bool PackHorizontally(std::vector<SubTexture>& subTextures, const IVec2& atlasSize) {
+        IVec2 subTexturePosition {0, 0};
+        auto yMax = 0;
+        
+        for (auto& subTexture: subTextures) {
+            auto subTextureSize = subTexture.mImage->GetSize();
+            if (!TextureFits(subTexturePosition, subTextureSize, atlasSize)) {
+                subTexturePosition = {0, yMax};
+                if (!TextureFits(subTexturePosition, subTextureSize, atlasSize)) {
+                    return false;
+                }
+            }
+            
+            subTexture.mPosition = subTexturePosition;
+            
+            subTexturePosition.x += subTextureSize.x;
+            if (subTexturePosition.y + subTextureSize.y > yMax) {
+                yMax = subTexturePosition.y + subTextureSize.y;
+            }
+        }
+        
+        return true;
+    }
+
+    Optional<IVec2> PackTexturesHorizontally(std::vector<SubTexture>& subTextures) {
+        IVec2 atlasImageSize {minSize, minSize};
+        auto counter = 1;
+        
+        while (atlasImageSize.x <= maxSize && atlasImageSize.y <= maxSize) {
+            if (PackHorizontally(subTextures, atlasImageSize)) {
+                return atlasImageSize;
+            }
+            
+            if (counter % 2 == 0) {
+                atlasImageSize.y *= 2;
+            } else {
+                atlasImageSize.x *= 2;
+            }
+            
+            ++counter;
+        }
+
+        return Optional<IVec2> {};
+    }
+
+    Optional<IVec2> PackTextures(std::vector<SubTexture>& subTextures,
+                                 TexturePackingDirection packingDirection) {
+        switch (packingDirection) {
+            case TexturePackingDirection::Horizontal: {
+                auto atlasImageSize = PackTexturesHorizontally(subTextures);
+                if (atlasImageSize.HasValue()) {
+                    return atlasImageSize;
+                }
+                return PackTexturesVertically(subTextures);
+            }
+            case TexturePackingDirection::Vertical: {
+                auto atlasImageSize = PackTexturesVertically(subTextures);
+                if (atlasImageSize.HasValue()) {
+                    return atlasImageSize;
+                }
+                return PackTexturesHorizontally(subTextures);
+            }
+        }
+    }
+
+    std::unique_ptr<IImage>
+    CreateAtlasImageAndCalcUVs(std::vector<SubTextureUVs> subTextureUVs,
+                               const std::vector<SubTexture>& subTexturePositions,
+                               const IVec2& atlasImageSize) {
+        return nullptr;
     }
 }
 
@@ -50,74 +177,16 @@ std::unique_ptr<IImage> TextureAtlas::CreateAtlasImage(const SubImages& images,
     
     auto atlasImageSize = PackTextures(subTextures, packingDirection);
     if (atlasImageSize.HasValue()) {
-        return CreateAtlasImage(subTextures, atlasImageSize.GetValue());
+        return CreateAtlasImageAndCalcUVs(mSubTextureUVs, subTextures, atlasImageSize.GetValue());
     }
     
     return nullptr;
 }
 
-Optional<IVec2> TextureAtlas::PackTextures(std::vector<SubTexture>& subTextures,
-                                           TexturePackingDirection packingDirection) {
-    switch (packingDirection) {
-        case TexturePackingDirection::Horizontal:
-            return PackTexturesHorizontally(subTextures);
-        case TexturePackingDirection::Vertical:
-            return PackTexturesVertically(subTextures);
-    }
-}
-
-Optional<IVec2> TextureAtlas::PackTexturesHorizontally(std::vector<SubTexture>& subTextures) {
-
-    return Optional<IVec2> {};
-}
-
-Optional<IVec2> TextureAtlas::PackTexturesVertically(std::vector<SubTexture>& subTextures) {
-    IVec2 size {minSize, minSize};
-    auto numTries = 0;
-    
-    while (size.x <= maxSize && size.y <= maxSize) {
-        if (PackVertically(subTextures, size)) {
-            return size;
-        }
-        
-        if (numTries % 2 == 0) {
-            size.y = size.y << 1;
-        } else {
-            size.x = size.x << 1;
-        }
-        
-        ++numTries;
-    }
-
-    return Optional<IVec2> {};
-}
-
-bool TextureAtlas::PackVertically(std::vector<SubTexture>& subTextures, const IVec2& atlasSize) {
-    IVec2 subTexturePosition {0, 0};
-    auto xMax = 0;
-    
-    for (auto& subTexture: subTextures) {
-        auto subTextureSize = subTexture.mImage->GetSize();
-        if (!TextureFitsInAtlas(subTexturePosition, subTextureSize, atlasSize)) {
-            subTexturePosition = {xMax, 0};
-            if (!TextureFitsInAtlas(subTexturePosition, subTextureSize, atlasSize)) {
-                return false;
-            }
-        }
-        
-        subTexture.mPosition = subTexturePosition;
-        
-        subTexturePosition.y += subTextureSize.y;
-        if (subTexturePosition.x + subTextureSize.x > xMax) {
-            xMax = subTexturePosition.x + subTextureSize.x;
-        }
+const SubTextureUVs* TextureAtlas::GetSubTextureUVs(int subTextureIndex) const {
+    if (subTextureIndex < mSubTextureUVs.size()) {
+        return &mSubTextureUVs[subTextureIndex];
     }
     
-    return true;
-}
-
-std::unique_ptr<IImage>
-TextureAtlas::CreateAtlasImage(const std::vector<SubTexture>& texturePositions,
-                               const IVec2& atlasImageSize) {
     return nullptr;
 }
--- Src/PhotonBeamEngine/Renderer/Common/TextureAtlas.hpp
+++ Src/PhotonBeamEngine/Renderer/Common/TextureAtlas.hpp
@@ -15,35 +15,22 @@ namespace Pht {
     };
     
     using SubImages = std::vector<std::unique_ptr<const IImage>>;
-    
+
+    struct SubTextureUVs {
+        Vec2 mBottomLeft;
+        Vec2 mBottomRight;
+        Vec2 mTopRight;
+        Vec2 mTopLeft;
+    };
+
     class TextureAtlas {
     public:
         std::unique_ptr<IImage> CreateAtlasImage(const SubImages& images,
                                                  TexturePackingDirection packingDirection);
         
-        struct SubTextureUVs {
-            Vec2 mBottomLeft;
-            Vec2 mBottomRight;
-            Vec2 mTopRight;
-            Vec2 mTopLeft;
-        };
-        
         const SubTextureUVs* GetSubTextureUVs(int subTextureIndex) const;
         
     private:
-        struct SubTexture {
-            IVec2 mPosition;
-            const IImage* mImage;
-        };
-        
-        Optional<IVec2> PackTextures(std::vector<SubTexture>& subTextures,
-                                     TexturePackingDirection packingDirection);
-        Optional<IVec2> PackTexturesHorizontally(std::vector<SubTexture>& subTextures);
-        Optional<IVec2> PackTexturesVertically(std::vector<SubTexture>& subTextures);
-        bool PackVertically(std::vector<SubTexture>& subTextures, const IVec2& size);
-        std::unique_ptr<IImage> CreateAtlasImage(const std::vector<SubTexture>& subTextures,
-                                                 const IVec2& atlasImageSize);
-        
         std::vector<SubTextureUVs> mSubTextureUVs;
     };
 }
