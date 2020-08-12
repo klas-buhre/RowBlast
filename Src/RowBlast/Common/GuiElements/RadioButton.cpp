03e51f17a4a599939f8f299bf633ffb50a61ec02
klas-buhre <klas.buhre81@gmail.com>
2020-08-12 20:16:21 +0200

    Added a radio button class.

---
+++ Src/RowBlast/Common/GuiElements/RadioButton.cpp
@@ -0,0 +1,60 @@
+#include "RadioButton.hpp"
+
+// Engine includes.
+#include "IEngine.hpp"
+#include "IAudio.hpp"
+#include "ISceneManager.hpp"
+#include "QuadMesh.hpp"
+#include "GuiView.hpp"
+
+// Game includes.
+#include "AudioResources.hpp"
+
+using namespace RowBlast;
+
+namespace {
+    std::unique_ptr<Pht::RenderableObject> CreateIconRenderable(Pht::IEngine& engine,
+                                                                const std::string& filename) {
+        Pht::Material iconMaterial {filename, 0.0f, 0.0f, 0.0f, 0.0f};
+        iconMaterial.SetBlend(Pht::Blend::Yes);
+        iconMaterial.SetAmbient(Pht::Color{0.95f, 0.95f, 0.95f});
+        
+        auto& sceneManager = engine.GetSceneManager();
+        return sceneManager.CreateRenderableObject(Pht::QuadMesh {1.7f, 1.7f, std::string{"radioButton"}},
+                                                   iconMaterial);
+    }
+}
+
+RadioButton::RadioButton(Pht::IEngine& engine,
+                         Pht::GuiView& view,
+                         Pht::SceneObject& clickableObject,
+                         const Pht::Vec3& positionInClickableObject,
+                         const Pht::Vec2& inputSize) :
+    mEngine {engine} {
+    
+    mButton = std::make_unique<Pht::Button>(clickableObject, inputSize, engine);
+    
+    mRadioButtonSceneObject = &view.CreateSceneObject();
+    mRadioButtonSceneObject->GetTransform().SetPosition(positionInClickableObject);
+    clickableObject.AddChild(*mRadioButtonSceneObject);
+    
+    mSelectedRenderable = CreateIconRenderable(engine, "radio_button_selected.png");
+    mNotSelectedRenderable = CreateIconRenderable(engine, "circle.png");
+    mRadioButtonSceneObject->SetRenderable(mNotSelectedRenderable.get());
+}
+
+bool RadioButton::IsClicked(const Pht::TouchEvent& event) {
+    auto isClicked = mButton->IsClicked(event);
+    if (isClicked) {
+        mEngine.GetAudio().PlaySound(static_cast<Pht::AudioResourceId>(SoundId::ButtonClick));
+        
+        mIsSelected = !mIsSelected;
+        if (mIsSelected) {
+            mRadioButtonSceneObject->SetRenderable(mSelectedRenderable.get());
+        } else {
+            mRadioButtonSceneObject->SetRenderable(mNotSelectedRenderable.get());
+        }
+    }
+    
+    return isClicked;
+}
---
+++ Src/RowBlast/Common/GuiElements/RadioButton.hpp
@@ -0,0 +1,40 @@
+#ifndef RadioButton_hpp
+#define RadioButton_hpp
+
+#include <memory>
+
+// Engine includes.
+#include "Button.hpp"
+#include "RenderableObject.hpp"
+
+namespace Pht {
+    class TouchEvent;
+    class GuiView;
+}
+
+namespace RowBlast {
+    class RadioButton {
+    public:
+        RadioButton(Pht::IEngine& engine,
+                    Pht::GuiView& view,
+                    Pht::SceneObject& clickableObject,
+                    const Pht::Vec3& positionInClickableObject,
+                    const Pht::Vec2& inputSize);
+        
+        bool IsClicked(const Pht::TouchEvent& event);
+        
+        bool IsSelected() {
+            return mIsSelected;
+        }
+        
+    private:
+        Pht::IEngine& mEngine;
+        std::unique_ptr<Pht::Button> mButton;
+        Pht::SceneObject* mRadioButtonSceneObject {nullptr};
+        std::unique_ptr<Pht::RenderableObject> mSelectedRenderable;
+        std::unique_ptr<Pht::RenderableObject> mNotSelectedRenderable;
+        bool mIsSelected {false};
+    };
+}
+
+#endif
